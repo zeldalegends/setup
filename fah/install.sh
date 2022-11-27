053029d2b76b42f9e9baec71a03a797ecb822884
alinonet <101158367+alinonet@users.noreply.github.com>
2022-11-27 04:09:50 +0100

    Update install.sh

--- fah/install.sh
+++ fah/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-arm64/v7.6/fahclient_7.6.21_arm64.deb
+wget https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.6/fahclient_7.6.21_amd64.deb
+# wget https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-arm64/v7.6/fahclient_7.6.21_arm64.deb # for ARM 
 wget https://download.foldingathome.org/releases/public/release/fahcontrol/debian-stable-64bit/v7.6/fahcontrol_7.6.21-1_all.deb
 
 sudo dpkg -i --force-depends fahclient_7.6.21_arm64.deb
