2efaf567c1de7dc91f42048be3d6e3262b8ac613
alinonet <101158367+alinonet@users.noreply.github.com>
2022-11-27 03:15:20 +0100

    Update sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-cat /etc/*release|grep DESCRIPTION
+#cat /etc/*release|grep DESCRIPTION
+uname -nrpv
 cat /proc/cpuinfo|grep name
 free -m
+docker ps -a --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"
+echo - e "Press \e[1;34mEnter\e[0m to continue..." 
+read key
+top -u ubuntu
